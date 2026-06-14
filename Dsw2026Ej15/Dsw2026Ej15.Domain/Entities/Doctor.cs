cb753f5119ab5baf5abf033d46461d97e23a407a
Sebastian Mieres <sebassegundomieres@gmail.com>
2026-06-14 15:12:43 -0300

    Se crea solucion, proyecto api, proyecto domain y proyecto data, hasta el punto 3.e incluido

---
+++ Dsw2026Ej15/Dsw2026Ej15.Domain/Entities/Doctor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej15.Domain.Entities
+{
+    public class Doctor
+    {
+        private string Name { get; set; }
+        private string LicenseNumber { get; set; }
+        private bool IsActive { get; set; }
+        private Speciality? speciality;
+    }
+}
---
+++ Dsw2026Ej15/Dsw2026Ej15.Domain/Entities/Speciality.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej15.Domain.Entities
+{
+    public class Speciality
+    {
+        private string Description { get; set; }
+        private string Name { get; set; }
+
+    }
+}
