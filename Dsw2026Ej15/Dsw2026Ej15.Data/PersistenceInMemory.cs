d8636094c966b70c69787583911ad5bd6e5d6d83
Maximo404 <MaximoNahimSuleiman@alu.frt.utn.edu.ar>
2026-06-14 16:00:16 -0300

    Punto 3.f completdo

---
+++ Dsw2026Ej15/Dsw2026Ej15.Data/PersistenceInMemory.cs
@@ -0,0 +1,61 @@
+﻿using Dsw2026Ej15.Domain.Entities;
+using Dsw2026Ej15.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace Dsw2026Ej15.Data
+{
+    public class PersistenceInMemory : IPersistence
+    {
+        public List<Doctor> Doctors { get; private set; } = new ();
+        public List<Speciality> Specialities { get; private set; } = new();
+
+        public PersistenceInMemory()
+        {
+            LoadSpecialities();
+        }
+        private void LoadSpecialities()
+        {
+            string filePath = "specialities.json";
+
+            if (File.Exists(filePath))
+            {
+                var json = File.ReadAllText(filePath);
+                var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+                var data = JsonSerializer.Deserialize<List<Speciality>>(json, options);
+
+                if(data != null)
+                {
+                    Specialities = data;
+                }
+
+            }
+            else 
+            {
+                Console.WriteLine("Not found");
+            }
+        }
+
+        public void AddDoctor(Doctor doctor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Doctor> GetActiveDoctors()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Doctor? GetDoctorById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeactivateDoctor(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Dsw2026Ej15/Dsw2026Ej15.Domain/Interfaces/IPersistence.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dsw2026Ej15.Domain.Entities;
+
+namespace Dsw2026Ej15.Domain.Interfaces
+{
+    public interface IPersistence
+    {
+        List<Doctor> Doctors {  get; }
+
+        List<Speciality> Specialities { get; }
+
+        void AddDoctor(Doctor doctor);
+
+        List<Doctor> GetActiveDoctors();
+
+        Doctor? GetDoctorById(Guid id);
+
+        bool DeactivateDoctor(Guid id);
+
+
+    }
+}
