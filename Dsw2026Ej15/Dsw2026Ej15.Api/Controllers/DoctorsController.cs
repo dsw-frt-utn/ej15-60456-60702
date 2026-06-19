164b677218dfb4b419143cb43ab8222aeb6fd560
Maximo404 <MaximoNahimSuleiman@alu.frt.utn.edu.ar>
2026-06-19 19:09:57 -0300

    Cambiando returns por lanzameinto de excepciones

--- Dsw2026Ej15/Dsw2026Ej15.Api/Controllers/DoctorsController.cs
+++ Dsw2026Ej15/Dsw2026Ej15.Api/Controllers/DoctorsController.cs
@@ -26,11 +26,12 @@ public class DoctorsController : ControllerBase
         if(string.IsNullOrWhiteSpace(request.Name) || 
             string.IsNullOrWhiteSpace(request.LicenseNumber))
         {
-            return BadRequest("El nombre y la matrícula son requeridos");
+            
+            throw new ValidationException("El nombre y la matrícula son requeridos");
         }
         var speciality = _persistence.GetSpecialityById(request.SpecialityId);
-        if (speciality == null) { 
-            return BadRequest("La especialidad no existe");
+        if (speciality == null) {
+            throw new ValidationException("La especialidad no existe");
         }
 
         
@@ -63,7 +64,7 @@ public class DoctorsController : ControllerBase
         if (doctor == null || !doctor.IsActive)
         {
             throw new ValidationException("No existe doctor o esta inactivo");
-            return NotFound();
+            
         }
 
         return Ok(doctor);
@@ -75,7 +76,7 @@ public class DoctorsController : ControllerBase
         if (_persistence.GetDoctorById(id) == null || _persistence.GetDoctorById(id).IsActive == false)
         {
             throw new ValidationException("No existe doctor o esta inactivo");
-            return NotFound();
+            
         }
 
         _persistence.DeactivateDoctor(id);
