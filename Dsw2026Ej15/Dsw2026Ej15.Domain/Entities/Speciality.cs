89bdf1b9bac4831380422d40543fdfaae18806d4
Sebastian Mieres <sebassegundomieres@gmail.com>
2026-06-24 14:36:05 -0300

    Mismo que anterior

--- Dsw2026Ej15/Dsw2026Ej15.Domain/Entities/Speciality.cs
+++ Dsw2026Ej15/Dsw2026Ej15.Domain/Entities/Speciality.cs
@@ -6,13 +6,16 @@ namespace Dsw2026Ej15.Domain.Entities;
 
 public class Speciality : BaseEntity
 {
-    public Speciality(string name, string description, Guid id)
+    public string Name { get; init; }
+    public string Description { get; init; }
+    private Speciality() { }
+    public Speciality(string name, string description, Guid? id = null)
+        : base(id)  
     {
         Name = name;
         Description = description;
-        Id = id;
+       
     }
 
-    public string Name { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
+    
 }
