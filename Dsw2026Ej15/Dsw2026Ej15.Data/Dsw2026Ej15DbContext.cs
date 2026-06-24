3755a84fef72bc4a0655bc47df40a6026ca8d3be
Sebastian Mieres <sebassegundomieres@gmail.com>
2026-06-24 13:51:19 -0300

    Instalo EF Core y creo el contexto

---
+++ Dsw2026Ej15/Dsw2026Ej15.Data/Dsw2026Ej15DbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej15.Data
+{
+    public class Dsw2026Ej15DbContext : DbContext
+    {
+    }
+}
