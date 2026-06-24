c55e5461909bad60d2bfdc1606aa95160d19a5da
Sebastian Mieres <sebassegundomieres@gmail.com>
2026-06-24 14:08:54 -0300

    Configuro contexto con la cadena de conexion

--- Dsw2026Ej15/Dsw2026Ej15.Api/Program.cs
+++ Dsw2026Ej15/Dsw2026Ej15.Api/Program.cs
@@ -1,6 +1,7 @@
 using Dsw2026Ej15.Api.Middlewares;
 using Dsw2026Ej15.Data;
 using Dsw2026Ej15.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Dsw2026Ej15.Api
 {
@@ -10,7 +11,12 @@ namespace Dsw2026Ej15.Api
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            var conectionString = "Data Source=(localdb)\\MSSQLLocalDB;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True";
+
+            builder.Services.AddDbContext<Dsw2026Ej15DbContext>(options =>
+            {
+                options.UseSqlServer(conectionString);
+            });
 
             builder.Services.AddControllers();
             
