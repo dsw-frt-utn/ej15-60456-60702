04b61ab079461b1efb0ea61e7eb74d212c8929d0
Sebastian Mieres <sebassegundomieres@gmail.com>
2026-06-24 14:15:04 -0300

    Agrego los DbSet

--- Dsw2026Ej15/Dsw2026Ej15.Data/Dsw2026Ej15DbContext.cs
+++ Dsw2026Ej15/Dsw2026Ej15.Data/Dsw2026Ej15DbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Dsw2026Ej15.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -11,6 +12,7 @@ namespace Dsw2026Ej15.Data
         {
 
         }
-        
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Speciality> Specialities { get; set; }
     }
 }
